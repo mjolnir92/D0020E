c4996b67789e530d0f1343cbbbfcd80d43489a40
Muhannes <haneda-3@student.ltu.se>
2016-01-29 11:15:40 +0100

    added  column for searching workers

--- CCNJS/public/javascripts/real-time.js
+++ CCNJS/public/javascripts/real-time.js
@@ -1,14 +1,36 @@
-$(".icon").on("click", function () {
+var realTime = function(){
 
-  var sensor = $(this).parent().attr("data-id");
+  $(".icon").on("click", function () {
 
-  //Server call to get data from sensor (AJAX?)
+    var sensor = $(this).parent().attr("data-id");
+
+    //Server call to get data from sensor (AJAX?)
 
 
 
 
-  //display data in graph
-  $('#myModal').modal('toggle');
-  $('.modal-title').html(sensor);
-  $('.modal-body').html("<p>Graph for "+sensor+"</p>");
-});
+    //display data in graph
+    $('#myModal').modal('toggle');
+    $('.modal-title').html(sensor);
+    $('.modal-body').html("<p>Graph for "+sensor+"</p>");
+  });
+
+  function minimizeNavBar(){
+    $("#indexmeny").css('width', "10%");
+    $("#mainDiv").css('margin-left', "10%");
+    $("#mainDiv").css('width', "90%");
+  }
+
+  function maximizeNavBar(){
+    $("#indexmeny").css('width', "15%");
+    $("#mainDiv").css('margin-left', "15%");
+    $("#mainDiv").css('width', "85%");
+  }
+
+  return {minimizeNavBar: minimizeNavBar,
+        maximizeNavBar: maximizeNavBar};
+}();
+
+
+
+
