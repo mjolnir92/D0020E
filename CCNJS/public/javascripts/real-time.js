9d3dbaae288720ca437fb7a65fe603f9b3519fbc
Magnus Bjork <mugsan@gmail.com>
2016-02-08 13:18:39 +0100

    ojoj nu ar det brottom

--- CCNJS/public/javascripts/real-time.js
+++ CCNJS/public/javascripts/real-time.js
@@ -45,26 +45,12 @@ var realTime = function(d3){
     $("#graph-area").html("");
     var data = phoneData.sensorData;
 
-    /*if(sensor == "heartrate") { // check wich sensor it should get data from
-      var data = [
-        {date: "1-May-12", close: "58.13"}, // dummy data
-        {date: "30-Apr-12", close: "53.98"},
-        {date: "27-Apr-12", close: "67.00"},
-        {date: "26-Apr-12", close: "89.70"},
-        {date: "25-Apr-12", close: "99.00"}
-      ];
-    }
-    else{//atm it shows same data for each sensor...
-      var data = [
-        {date: "1-May-12", close: "58.13"},
-        {date: "30-Apr-12", close: "53.98"},
-        {date: "27-Apr-12", close: "67.00"},
-        {date: "26-Apr-12", close: "89.70"},
-        {date: "25-Apr-12", close: "99.00"}
-      ];
-    }*/
-  //margins for graph
-    var margin = {top: 30, right: 20, bottom: 30, left: 50},
+    var margin = {
+            top: 30,
+            right: 20,
+            bottom: 30,
+            left: 50
+        },
         width = 600 - margin.left - margin.right,
         height = 270 - margin.top - margin.bottom;
 
@@ -73,15 +59,19 @@ var realTime = function(d3){
     var y = d3.scale.linear().range([height, 0]);
 
     // Parse the date / time... does not work!
-    var parseDate = d3.time.format("%Y-%m-%dT%H:%M:%S").parse;
+    var parseDate = d3.time.format.iso.parse;
 
 
 // Define the axes
-    var xAxis = d3.svg.axis().scale(x)
-        .orient("bottom").ticks(5);
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom")
+        .ticks(5);
 
-    var yAxis = d3.svg.axis().scale(y)
-        .orient("left").ticks(5);
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .ticks(5);
 
 // Define the line
     var valueline = d3.svg.line()
@@ -98,9 +88,7 @@ var realTime = function(d3){
             "translate(" + margin.left + "," + margin.top + ")");
 
     data.forEach(function(d) {
-      console.log(d.time);
       d.time = parseDate(d.time);
-      console.log(d.time);
       d[sensor] = +d[sensor];
     });
 
@@ -124,7 +112,7 @@ var realTime = function(d3){
   }
 
   client.socket.on( 'phoneData', function(phoneData){
-    makeRealTimeGraph(phoneData, sensor);
+      makeRealTimeGraph(phoneData, sensor);
   } );
 
   return {minimizeNavBar: minimizeNavBar,
