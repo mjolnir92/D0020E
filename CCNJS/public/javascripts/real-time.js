6ed6606b7cdf3dc0fb8cc3b5aa0a4fda551758b7
Muhannes <haneda-3@student.ltu.se>
2016-02-08 13:02:11 +0100

    getting d3js to work

--- CCNJS/public/javascripts/real-time.js
+++ CCNJS/public/javascripts/real-time.js
@@ -1,8 +1,8 @@
 var realTime = function(d3){
-
+  var sensor = "pulse";
   $(".icon").on("click", function () {
 
-    var sensor = $(this).parent().attr("data-id");
+    sensor = $(this).parent().attr("data-id");
 
     //Server call to get data from sensor (AJAX?)
 
@@ -12,7 +12,6 @@ var realTime = function(d3){
     //display data in graph
     $('#myModal').modal('toggle');
     $('.modal-title').html(sensor);
-    makeRealTimeGraph();
     $(function() {
       $( "#datepicker1" ).datepicker();
       $( "#datepicker2" ).datepicker();
@@ -42,9 +41,11 @@ var realTime = function(d3){
   }
 
   //graph with d3js
-  function makeRealTimeGraph(sensor) {
+  function makeRealTimeGraph(phoneData, sensor) {
     $("#graph-area").html("");
-    if(sensor == "heartrate") { // check wich sensor it should get data from
+    var data = phoneData.sensorData;
+
+    /*if(sensor == "heartrate") { // check wich sensor it should get data from
       var data = [
         {date: "1-May-12", close: "58.13"}, // dummy data
         {date: "30-Apr-12", close: "53.98"},
@@ -61,7 +62,7 @@ var realTime = function(d3){
         {date: "26-Apr-12", close: "89.70"},
         {date: "25-Apr-12", close: "99.00"}
       ];
-    }
+    }*/
   //margins for graph
     var margin = {top: 30, right: 20, bottom: 30, left: 50},
         width = 600 - margin.left - margin.right,
@@ -71,8 +72,8 @@ var realTime = function(d3){
     var x = d3.time.scale().range([0, width]);
     var y = d3.scale.linear().range([height, 0]);
 
-    // Parse the date / time
-    var parseDate = d3.time.format("%d-%b-%y").parse;
+    // Parse the date / time... does not work!
+    var parseDate = d3.time.format("%Y-%m-%dT%H:%M:%S").parse;
 
 
 // Define the axes
@@ -84,8 +85,8 @@ var realTime = function(d3){
 
 // Define the line
     var valueline = d3.svg.line()
-        .x(function(d) { return x(d.date); })
-        .y(function(d) { return y(d.close); });
+        .x(function(d) { return x(d.time); })
+        .y(function(d) { return y(d[sensor]); });
 
 
     var svg = d3.select("#graph-area") //create the svg in the modal-body
@@ -97,13 +98,15 @@ var realTime = function(d3){
             "translate(" + margin.left + "," + margin.top + ")");
 
     data.forEach(function(d) {
-      d.date = parseDate(d.date);
-      d.close = +d.close;
+      console.log(d.time);
+      d.time = parseDate(d.time);
+      console.log(d.time);
+      d[sensor] = +d[sensor];
     });
 
       // Scale the range of the data
-      x.domain(d3.extent(data, function(d) { return d.date; }));
-      y.domain(d3.extent(data, function(d) { return d.close; }));
+      x.domain(d3.extent(data, function(d) { return d.time; }));
+      y.domain(d3.extent(data, function(d) { return d[sensor]; }));
 
       svg.append("path")		// Add the valueline path.
           .attr("class", "line")
@@ -119,6 +122,10 @@ var realTime = function(d3){
           .call(yAxis);
 
   }
+
+  client.socket.on( 'phoneData', function(phoneData){
+    makeRealTimeGraph(phoneData, sensor);
+  } );
 
   return {minimizeNavBar: minimizeNavBar,
         maximizeNavBar: maximizeNavBar};
