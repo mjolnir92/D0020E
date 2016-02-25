f7974f4a6ee55b8a0a5196032b5c9b13fe7552f3
Magnus Bjork <mugsan@gmail.com>
2016-02-25 10:45:15 +0100

    party hard

--- Phone/connection.js
+++ Phone/connection.js
@@ -3,10 +3,32 @@
  * Created by magnusbjork on 2/25/16.
  */
 
-module.exports = function( http ){
-    return{
-        push: function( json ){
+module.exports = function( http, options ){
 
+
+
+    return{
+        post: function( path, json ){
+            var data = JSON.stringify(json);
+            var options = {
+                host: options.host,
+                port: options.port,
+                path: path,
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Content-Length': Buffer.byteLength(data)
+                }
+            };
+
+            var req = http.request(options, function(res) {
+                res.setEncoding('utf8');
+                res.on('data', function (chunk) {
+                    console.log("body: " + chunk);
+                });
+            });
+            req.write(data);
+            req.end();
         }
     }
 };
--- Phone/socket.js
+++ Phone/socket.js
@@ -3,8 +3,10 @@
  * Created by magnusbjork on 2/25/16.
  */
 
-module.exports = function( io ) {
+module.exports = function( io, server ) {
     io.on( 'connection', function( socket ) {
+        var data = { hello: 'world' };
+        server.push( data );
         console.log( 'a user connected' );
         socket.on( 'disconnect', function(){
             console.log( 'user disconnected' );
