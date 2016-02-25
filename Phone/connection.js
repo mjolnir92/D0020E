b4b2998653832d734ec9c96816ddcd637aa4f2ca
Magnus Bjork <mugsan@gmail.com>
2016-02-25 09:54:47 +0100

    minor changes, forgettable...

---
+++ Phone/connection.js
@@ -0,0 +1,12 @@
+/**
+ *
+ * Created by magnusbjork on 2/25/16.
+ */
+
+module.exports = function( http ){
+    return{
+        push: function( json ){
+
+        }
+    }
+};
--- Phone/public/js/client.js
+++ Phone/public/js/client.js
@@ -3,7 +3,7 @@
  * Created by magnusbjork on 2/25/16.
  */
 
-var client = function( socket ) {
+var client = function( socket, http ) {
     socket.on( 'connection', function( argument ) {
         console.log( 'connected' );
         console.log( argument );
