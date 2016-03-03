be8876ec3313aedb3257775f94d63bf41a1cf0bb
Magnus Bjork <mugsan@gmail.com>
2016-03-03 10:39:56 +0100

    gjort lite gran idag,

--- CCNJS/app.js
+++ CCNJS/app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -8,7 +7,6 @@ var hbs = require( 'hbs' );
 var fs = require( 'fs' );
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -17,7 +15,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -33,7 +30,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.use('/', routes);
-app.use('/users', users);
 
 //Register partials
 
--- CCNJS/routes/index.js
+++ CCNJS/routes/index.js
@@ -69,17 +69,8 @@ router.get('/', function(req, res, next) {
   res.render('real-time');
 });
 
-router.get('/client', function(req, res, next){
-  res.render('client', { value: 400 });
-});
-
 router.get('/storedData', function(req, res, next){
   res.render('storedData');
-});
-
-router.get('/logs/relay', function(req, res, next){
-  console.log(__dirname);
-  res.sendFile(path.resolve(path.join(__dirname,'../public/logs/relay.log')));
 });
 
 router.get('/real-time', function(req, res, next){
@@ -89,18 +80,6 @@ router.get('/real-time', function(req, res, next){
 router.get('/alarms', function(req, res, next){
   res.render('alarms');
 });
-
-router.get('/logs/ctrl', function(req, res, next){
-  res.sendFile(path.resolve(path.join(__dirname,'../public/logs/ctrl.log')));
-});
-
-router.get('/logs/content', function(req, res, next){
-  res.sendFile(path.resolve(path.join(__dirname,'../public/logs/content.log')));
-});
-
-router.get('/d3', function( req, res ) {
-    res.render( 'd3test' );
-} );
 
 //-----SQL------
 router.post('/search_alarms', function(req, res){
